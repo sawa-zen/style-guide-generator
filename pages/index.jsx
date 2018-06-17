6ff2104ef642d8e27888ac893ac3952dbca46290
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-06-18 03:20:35 +0900

    Add Home container

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,2 @@
-import * as React from 'react';
-import styled from 'styled-components';
-
-const Home = () => (
-  <Wrapper>Style Guide Generator</Wrapper>
-);
-
-const Wrapper = styled.div`
-  background: red;
-`;
-
+import Home from '../src/containers/Home';
 export default Home;
---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Home = () => (
+  <Wrapper>Style Guide Generator</Wrapper>
+);
+
+const Wrapper = styled.div`
+  background: red;
+`;
+
+export default Home;
