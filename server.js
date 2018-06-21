bb2fe1f64bda01d6e1c5a61b6d978cf1958bbcc8
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-06-22 03:36:35 +0900

    Download zip

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const next = require('next')
 const fs = require('fs')
+const Zip = require('node-zip')
 const { exec } = require('child_process');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const zip = new Zip;
 
 app.prepare()
   .then(() => {
@@ -21,11 +23,18 @@ app.prepare()
     })
 
     server.get('/export', (req, res) => {
-      exec('npm ', (err, stdout, stderr) => {
-        console.log(stdout);
+      exec('npm run skpm:build', (err, stdout, stderr) => {
         if (err) { console.log(err); }
-        var file = __dirname + '/styleguide.sketchplugin.zip';
-        res.download(file);
+
+        // zipファイルのストリームを生成して、archiverと紐付ける
+        var zip_file_name = 'styleguide.sketchplugin.zip';
+        zip.file('./styleguide.sketchplugin', 'Hello, World!');
+        var options = {base64: false, compression:'DEFLATE'};
+        fs.writeFile(zip_file_name, zip.generate(options), 'binary', function (error) {
+          console.log(`wrote ${zip_file_name}`, error);
+          var file = `${__dirname}/${zip_file_name}`;
+          res.download(file);
+        });
       });
     })
 
