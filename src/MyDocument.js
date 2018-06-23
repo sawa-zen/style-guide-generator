98632b579b602a0ad255c51e713e4920cd3b92df
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-06-24 02:40:12 +0900

    Fix globalCSS

--- src/MyDocument.js
+++ src/MyDocument.js
@@ -1,37 +1,43 @@
-import * as React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet, injectGlobal } from 'styled-components'
+import * as React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet, css } from 'styled-components'
 
-injectGlobal`
+class MyDocument extends Document {
+
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styleTags } // return styles collected
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+          <meta name="format-detection" content="telephone=no"/>
+          <meta name="msapplication-tap-highlight" content="no"/>
+          <style dangerouslySetInnerHTML={{ __html: globalCSS }} />
+          {this.props.styleTags}
+        </Head>
+
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </html>
+    )
+  }
+}
+
+const globalCSS = `
   html, body {
     margin: 0;
     padding: 0;
     font-family: 游ゴシック体, 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', メイリオ, Meiryo, Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
   }
 `;
-
-class MyDocument extends Document {
-  render () {
-    const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
-
-    return (
-      <html>
-        <Head>
-          <title>Style Guide Generator</title>
-          <link rel='icon' href='/static/favicon.png' />
-          {styleTags}
-        </Head>
-        <body>
-          <div className='root'>
-            {main}
-          </div>
-          <NextScript />
-        </body>
-      </html>
-    )
-  }
-}
 
 export default MyDocument;
